d2351922a6b38d3fba7146127dfb13f6ad58c7a2
Andy-Merhaut <andreasmerhaut11502@gmail.com>
2021-05-14 15:52:12 +0200

    table instance

---
+++ MaturitaDatabaseEdit/Bike.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaturitaDatabaseEdit
+{
+    public class Bike
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int Power { get; set; }
+    }
+}
