7a9e8f9712343ea5dbda03612e55703bd1862803
Andy-Merhaut <andreasmerhaut11502@gmail.com>
2021-05-14 15:52:06 +0200

    database connection

---
+++ MaturitaDatabaseEdit/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MaturitaDatabaseEdit
+{
+    [DbConfigurationType(typeof(MySql.Data.EntityFramework.MySqlEFConfiguration))]
+    public class MyContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
